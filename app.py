5e6961785d7e66dddc7075e04e20320cb57fad1f
Joel Lara <joel.lq@hotmail.com>
2016-04-05 22:14:37 +0000

    Fixed readme and app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    return "Te odio, Steve"
+    return "Hello World!"
     
 app.run(host = os.getenv("IP",'0.0.0.0'),port=int(os.getenv("PORT",8080)))
