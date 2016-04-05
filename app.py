6a873afc45714718618efe5ee0f1d70ea5c6117b
Joel Lara <joel.lq@hotmail.com>
2016-04-05 23:19:31 +0000

    Heorku

--- app.py
+++ app.py
@@ -7,4 +7,6 @@ app = Flask(__name__)
 def index():
     return "Hello World!"
     
-app.run(host = os.getenv("IP",'0.0.0.0'),port=int(os.getenv("PORT",8080)))
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
